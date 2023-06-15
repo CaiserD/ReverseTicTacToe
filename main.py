c35ab854ace7c860ceb3c35be7362fa315c87f41
CaiserD <ctl78787@gmail.com>
2023-06-16 00:46:43 +0900

    updated main.py

--- main.py
+++ main.py
@@ -6,26 +6,43 @@ WIN_SIZE = 900
 
 pg.init()
 
-class Game(): 
+
+class GameLogic:
+    def __init__(self, game):
+        self.game = game
+        self.field_image = self.get_scaled_image()
+
+    @staticmethod
+    def get_scaled_image(path, res):
+        img = pg.image.load(path)
+        return pg.transform.smoothscale(img, res)
+
+    def run(self):
+        pass
+
+
+class Game():
     def __init__(self):
         pg.init()
         self.screen = pg.display.set_mode([WIN_SIZE] * 2)
         self.clock = pg.time.Clock()
-    
-    #Check Exit from Application
-    def check_events(self): 
+        self.tic_tac_toe = GameLogic(self)
+
+    # Check Exit from Application
+    def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-    
-    
-    def run(self): #Run game
+
+    def run(self):  # Run game
         while True:
+            self.tic_tac_toe.run()
             self.check_events()
             pg.display.update()
             self.clock.tick(60)
-    
+
+
 if __name__ == "__main__":
     game = Game()
     game.run()
