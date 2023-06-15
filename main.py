620cf8ce1a7c27f4aad6d8f8ef8db28c670e768e
CaiserD <ctl78787@gmail.com>
2023-06-15 23:59:13 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-import pygame
+import pygame as pg
+import sys
+from random import randint
+
+WIN_SIZE = 900
 
 pygame.init()
 
-def main(): #Run game
+class Game(): 
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mod([WIN_SIZE] * 2)
+        self.clock = pg.time.Clock()
+    
+    #Check Exit from Application
+    def check_events(self): 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+    
+    
+    def run(self): #Run game
+        while True:
+            self.check_events()
+            pg.display.update()
+            self.clock.tick(60)
+    
+if __name__ = "__main__":
+    game = Game()
+    game.run()
